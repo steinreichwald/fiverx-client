92ed9a07a84953a7e2a3e3a428f424a3db84b90d
Felix Schwarz <felix.schwarz@schwarz.eu>
2022-03-28 09:12:41 +0200

    move requirements into setup.cfg

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python3
 
-import re
-import sys
-
 from setuptools import setup
 
-
-requires = []
-if sys.version_info < (2, 7):
-    requires.append('argparse')
-
-def requires_from_file(filename):
-    requirements = []
-    with open(filename, 'r') as requirements_fp:
-        for line in requirements_fp.readlines():
-            match = re.search('^\s*([a-zA-Z][^#]+?)(\s*#.+)?\n$', line)
-            if match:
-                requirements.append(match.group(1))
-    return requirements
-
-
-setup(
-    install_requires=requires_from_file('requirements.txt'),
-)
+setup()
 
